3b1398d010655a0af435cf8ed1aadc4b725f39ab
Audrius Skersys <109533899+a-skersys@users.noreply.github.com>
2022-10-21 13:34:54 +0200

    First commit

---
+++ data.js
@@ -0,0 +1,58 @@
+let data =  [
+
+                [
+                    "CSS", "Sass", "CSS Layouts comparision",
+                    [
+                        [ "Doesn't need compilation", "y", "n" ],
+                        [ "Easy to use", "y", "m" ],
+                        [ "Mixins", "n", "y" ],
+                        [ "Functions", "n", "y" ],
+                        [ "Indented syntax", "n", "y" ],
+                        [ "Nested syntax", "n", "y" ]
+                    ],
+                    "13.10.2022"
+                ],
+
+                [
+                    "Flexbox",
+                    "Grid",
+                    "CSS layouts comparision",
+                    [
+                        [ "Expands with content", "y", "n" ],
+                        [ "Great for responsive design", "y", "m" ],
+                        [ "Overlapping elements", "m", "y" ],
+                        [ "Two-directional", "n", "y"],
+                        [ "Great for compex layouts", "n", "y"],
+                    ],
+                    "11.10.2022"
+                ],
+
+                [
+                    "JavaScript", "TypeScript", "Programming Languages comparision",
+                    [
+                        [ "More information available", "y", "n" ],
+                        [ "Doesn't have to be compiled", "y", "n" ],
+                        [ "Easy to debug", "m", "y" ],
+                        [ "Suitable for complex applications", "n", "y"],
+                        [ "Fully compatible", "n", "y"] ,
+                        [ "Static typing", "n", "y" ],
+                        [ "Is used in most popular frameworks", "n", "y" ]
+                    ],
+                    "06.10.2022"
+                ],
+
+                [
+                    "React", "Vue", "Frontend Frameworks comparision",
+                    [
+                        [ "Popular in job market", "y", "n" ],
+                        [ "Scalable to large projects", "y", "m" ],
+                        [ "Based on Javascript", "y", "y" ],
+                        [ "Easy to learn", "m", "y" ],
+                        [ "HTML besides JSX", "n", "y"],
+                        [ "Two-way binding", "n", "y"],
+                        [ "Small project size", "n", "y"],
+                    ],
+                    "04.10.2022"
+                ],
+                
+            ]
---
+++ script.js
@@ -0,0 +1,179 @@
+const burger = document.getElementById("burger");
+const sidebar = document.getElementById("sidebar");
+let toggle = false;
+
+burger.addEventListener("click", function() {
+    if (!toggle) {
+        sidebar.style.transform = "translate(0px)";
+        burger.innerHTML = '<i class="fas fa-times"></i>';
+        toggle = true;
+    } else {
+        sidebar.style.transform = "translate(-250px)";
+        burger.innerHTML = '<i class="fas fa-bars"></i>';
+        toggle = false;
+    }
+
+}, false);
+
+const bigScreen = window.matchMedia('(min-width: 771px)');
+
+bigScreen.addEventListener("change", () => {
+    this.checkNative();
+    if (screen.matches) { // If media query matches
+        sidebar.style.transform = "translate(0px)";
+    } else {
+        sidebar.style.transform = "translate(-250px)";
+    }
+});
+
+
+function Card(name1, name2, description, comparision, date) {
+    this.name1 = name1;
+    this.name2 = name2;
+    this.description = description;
+    this.comparision = comparision;
+    this.date = date;
+    this.fill = function() {
+        container = document.getElementById("container");
+
+        const tile = document.createElement("div");
+        tile.setAttribute("class", "tile");
+        tile.setAttribute("id", `${this.name1}${this.name2}`);
+        container.appendChild(tile);
+
+        const frame = document.createElement("div");
+        frame.setAttribute("class", "frame");
+        frame.setAttribute("id", `frame${this.name1}${this.name2}`);
+        tile.appendChild(frame);
+
+        const currentFrame = document.getElementById(`frame${this.name1}${this.name2}`);
+
+        const header = document.createElement("header");
+        header.innerHTML = `<p>// <b>${this.name1}</b> vs. <b>${this.name2}</b></p>`;
+        currentFrame.appendChild(header);    
+
+        const main = document.createElement("main");
+        main.setAttribute("id", `main${this.name1}${this.name2}`);
+        currentFrame.appendChild(main);
+
+        const currentMain = document.getElementById(`main${this.name1}${this.name2}`);
+        const name1 = `${this.name1}`;
+        const name2 = `${this.name2}`;
+
+        const logoDiv = function(logoNum, node, name) {
+            let logo = document.createElement("div");
+            logo.setAttribute("class", `logo ${logoNum}`);
+            node.appendChild(logo);
+
+            let img = document.createElement("img");
+            img.setAttribute("src", `logos/${name}.svg`);
+            img.setAttribute("alt", `${name} logo`);
+            logo.appendChild(img);
+
+
+            let p = document.createElement("p");
+            p.textContent = name;
+            logo.appendChild(p);
+        }
+
+        logoDiv("Logo1", currentMain, name1);
+        logoDiv("Logo2", currentMain, name2);
+
+        const title = document.createElement("div");
+        title.setAttribute("class", "title");
+        currentMain.appendChild(title);
+        
+        const description = document.createElement("p");
+        description.textContent = this.description;
+        title.appendChild(description);
+
+        const contentDiv = document.createElement("div");
+        contentDiv.setAttribute("class", "content");
+        currentMain.appendChild(contentDiv);
+        
+        const ul = document.createElement("ul");
+        ul.setAttribute("id", `content${this.name1}${this.name2}`);
+        contentDiv.appendChild(ul);
+
+   
+        const content = document.getElementById(`content${this.name1}${this.name2}`);
+
+        this.comparision.forEach(element => {
+            const list = document.createElement('li');
+            const div = document.createElement('div');
+            const p = document.createElement('p');
+            const value1 = document.createElement('div');
+            const value2 = document.createElement('div');
+
+            value1.setAttribute("class", "value");
+            value2.setAttribute("class", "value");
+
+            const yes = '<span class="fa-stack"><i class="fas fa-circle fa-stack-1x"></i><i class="fas fa-check-circle fa-stack-1x"></i></span>';
+            const no = '<span class="fa-stack"><i class="fas fa-circle fa-stack-1x"></i><i class="fas fa-times-circle fa-stack-1x"></i></span>';
+            const maybe = '<span class="fa-stack"><i class="fas fa-circle fa-stack-1x fa-black"></i><i class="fas fa-info-circle fa-stack-1x"></i></span>';
+
+            let values = function(node, element) {
+                if (element == "y") {
+                    node.innerHTML = yes;
+                } else if (element == "n") {
+                    node.innerHTML = no;
+                } else if (element == "m") {
+                    node.innerHTML = maybe;
+                }
+            }
+
+            values(value1, element[1]);
+            values(value2, element[2]);
+
+            p.textContent = element[0];
+
+            content.appendChild(list);
+            list.appendChild(div);
+            div.appendChild(p);
+            list.appendChild(value1);
+            list.appendChild(value2);
+        });
+
+        const footer = document.createElement("footer");
+        footer.innerHTML = '<div class="learnWithSkersys"><img src="learnWithSkersys.svg"><p class="hashtag">#learnWithSkersys</p></div><div class="website"><p><i class="fas fa-desktop"></i> learnwith.skersys.eu</p></div>'
+        currentFrame.appendChild(footer); 
+
+        const tags = document.createElement("div");
+        tags.setAttribute("class", "tags");
+        tile.appendChild(tags); 
+        
+        const date = document.createElement("div");
+        date.setAttribute("class", "date");
+        date.textContent = `${this.date}`;
+        tags.appendChild(date); 
+
+        const download = document.createElement("div");
+        download.setAttribute("class", "download");
+        download.innerHTML = `<a href="images\/${this.name1}${this.name2}.png" download><i class="fas fa-file-download"></i> PNG</a>`;
+        tags.appendChild(download); 
+    }
+}
+
+const menu = document.getElementById("menu");
+
+
+data.forEach(element => {
+    const index = data.indexOf(element);
+    const myData = data[index];
+    const newCard = new Card(myData[0], myData[1], myData[2], myData[3], myData[4]);
+    newCard.fill();
+
+    const a = document.createElement('a');
+    a.setAttribute("href", `#${data[index][0]}${data[index][1]}`);
+    menu.appendChild(a);
+
+    const li = document.createElement('li');
+    li.innerHTML = `<p><nobr><img src="logos/${data[index][0]}.svg" width="12px"> ${data[index][0]}</nobr> <span style="color: #999999">vs.</span> <nobr><img src="logos/${data[index][1]}.svg" width="12px">&nbsp;${data[index][1]}</nobr></p>`;
+    a.appendChild(li);
+
+});
+
+
+
+
+
